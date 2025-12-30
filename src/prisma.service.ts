d1fac3138ecb7c520e307822a53671187317df3e
ymetdev <tembadboi@gmail.com>
2025-12-30 15:00:04 +0700

    add password for docker

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -5,12 +5,13 @@ import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
+    // prisma.service.ts
     const adapter = new PrismaMariaDb({
       host: 'localhost',
       port: 3306,
       user: 'root',
-      password: '', // ถ้าไม่มีรหัสผ่านให้ใส่ ""
-      database: 'll2_hotel', // <--- ต้องมีบรรทัดนี้ เพื่อบอกชื่อ DB ที่จะใช้งาน
+      password: 'root', // <--- ใส่รหัสผ่าน "root" ลงไปตรงนี้
+      database: 'll2_hotel',
       connectionLimit: 5,
     });
     super({ adapter });
